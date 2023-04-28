02749c35870c4bdeb38c8303285dc67fcc096822
egbor <egborosebhulimen8@gmail.com>
2023-04-28 04:23:40 -0300

    Fixed help menu link

--- create_bin.sh
+++ create_bin.sh
@@ -32,7 +32,7 @@ cat alias_Recycle_bin.sh > ~/.$aliaseSymbols/alias_Recycle_bin.sh
 cat man_Recycle_bin.sh > ~/.$aliaseSymbols/man_Recycle_bin.sh
 
 # makes all the .sh file an executable
-all_sh_files=$(ls | find -name "*.sh" | cut -c 3- | xargs echo)
+all_sh_files=$(ls | find -name "~/.$aliaseSymbols/*.sh" | cut -c 3- | xargs echo)
 
 for sh in $all_sh_files; do 
 				chmod 755 $sh
