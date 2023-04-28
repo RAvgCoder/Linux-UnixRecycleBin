d9197ad294bc9dc49ac0f75943040da8170f3aee
egbor <egborosebhulimen8@gmail.com>
2023-04-28 03:43:32 -0300

    Fixed issues for machines who do not have the .bashrc file

--- create_bin.sh
+++ create_bin.sh
@@ -6,7 +6,7 @@ if ! [ -f ~/.bashrc ]; then
 				echo "# You may want to put all your additions into a separate file like" >> ~/.bashrc
 				echo "# ~/.bash_aliases, instead of adding them here directly." >> ~/.bashrc
 				echo "if [ -f ~/.bash_aliases ]; then" >> ~/.bashrc
-				echo "	. ~/.bash_aliases" >> ~/.bashrc
+				echo "	.~/.bash_aliases" >> ~/.bashrc
 				echo "fi" >> ~/.bashrc
 fi
 
